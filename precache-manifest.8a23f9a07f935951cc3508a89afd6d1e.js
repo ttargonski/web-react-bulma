848c66b441474686d6575e18e4cf846d401b2618
Tomasz Targoński <tomasz.targonski95@gmail.com>
2020-03-03 22:16:05 +0100

    Updates

---
+++ precache-manifest.8a23f9a07f935951cc3508a89afd6d1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d128d3c990ffb2ccd4a175615283bbd",
+    "url": "/web-react-bulma/index.html"
+  },
+  {
+    "revision": "a8a6d393e41b96723d06",
+    "url": "/web-react-bulma/static/css/2.d2e2750e.chunk.css"
+  },
+  {
+    "revision": "07091fe73ecb94bc694a",
+    "url": "/web-react-bulma/static/css/main.3985727d.chunk.css"
+  },
+  {
+    "revision": "a8a6d393e41b96723d06",
+    "url": "/web-react-bulma/static/js/2.a051439a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/web-react-bulma/static/js/2.a051439a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07091fe73ecb94bc694a",
+    "url": "/web-react-bulma/static/js/main.926a3f89.chunk.js"
+  },
+  {
+    "revision": "6390d6925981aa1c6895",
+    "url": "/web-react-bulma/static/js/runtime-main.4da40c7b.js"
+  },
+  {
+    "revision": "5b2b4e1b05be37f39059e11fcf3a1d79",
+    "url": "/web-react-bulma/static/media/clouds-1.5b2b4e1b.jpg"
+  },
+  {
+    "revision": "432fec8afd9288947c03373550bd6ac3",
+    "url": "/web-react-bulma/static/media/company.432fec8a.jpg"
+  }
+]);
